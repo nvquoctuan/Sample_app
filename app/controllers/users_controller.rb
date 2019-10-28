c5efd4785df1855b930d2b47a31012260f5c4205
nvquoctuan <nvquoctuan@gmail.com>
2019-10-28 09:25:06 +0700

    chapter_8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
       flash[:success] = t ".welcome"
       redirect_to @user
     else
@@ -25,6 +26,7 @@ class UsersController < ApplicationController
   def load_user
     @user = User.find_by(id: params[:id])
     return if @user
+
     flash[:danger] = t ".danger"
     redirect_to root_url
   end
